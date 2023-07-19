8935cb3c40e3725dd7cfd1bf6a217d05986f9760
muhammad <mo.alidadi@gmail.com>
2023-07-19 04:32:24 +0330

    finish mifahost page

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('auth/', TemplateView.as_view(template_name='./Auth/Auth.html'), name='auth'),
     path('profile/', TemplateView.as_view(template_name='./Profile/Profile.html'), name='profile'),
     path('artist/', TemplateView.as_view(template_name='./Artist/Artist.html'), name='artist'),
-    path('aucation/', TemplateView.as_view(template_name='./Aucation/Aucation.html'), name='aucation'),
+    path('mifaaucation/', TemplateView.as_view(template_name='./Aucation/Aucation.html'), name='aucation'),
     path('mifahost/', TemplateView.as_view(template_name='./MIFAhost/MIFAhost.html'), name='mifahost'),
 
 
